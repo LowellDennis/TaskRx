680aeacad947d32f47f206563fa6e65c0a345d86
Lowell Dennis <lowell.dennis@hpe.com>
2025-06-27 03:59:33 -0500

    V1.0.5

--- TaskRx.Designer.cs
+++ TaskRx.Designer.cs
@@ -356,7 +356,7 @@
             Controls.Add(txtOutput);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "TaskRx";
-            Text = "TaskRx V1.0.4";
+            Text = "TaskRx V1.0.5";
             Load += TaskRx_Load;
             executionStrip.ResumeLayout(false);
             executionStrip.PerformLayout();
