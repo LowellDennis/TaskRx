535c5977c0105643cf12edd28d440b95980035c6
Lowell Dennis <Lowell.Dennis@hpe.com>
2025-10-15 16:19:48 -0500

    v1.0.7

--- BuildInstaller.bat
+++ BuildInstaller.bat
@@ -7,7 +7,7 @@ echo Creating installer...
 echo Please make sure Inno Setup is installed and iscc.exe is in your PATH
 
 REM Update the path to your Inno Setup Compiler if needed
-iscc TaskRxSetup.iss
+ "C:\Program Files (x86)\Inno Setup 6"\iscc TaskRxSetup.iss
 
 echo.
 echo Done! The installer should be created in the current directory as TaskRxSetup.exe
